cebdfade22634cdef88780602afd780a2065d7de
Jean Sebastian French <sebas03446@gmail.com>
2024-10-14 21:58:46 +0200

    feat: add button and input

---
+++ frontend/src/views/ChatView.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from 'vue';
+import ChatInput from '@/components/chatInput.vue';
+import ChatButton from '@/components/chatButton.vue';
+
+const inputValue = ref('');
+
+const handleButtonClick = () => {
+    console.log('Button clicked!');
+};
+
+
+</script>
+
+<template>
+    <div class="flex w-full h-full justify-center">
+        <div class="fixed bottom-0 justify-center p-14 w-full">
+            <div class="test flex justify-center space-x-2 max-h-64 w-full">
+                <ChatInput v-model="inputValue" class="w-1/2"  />
+                <ChatButton @click="handleButtonClick" class="max-h-12 self-end" />
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<style scoped></style>
